d6dab4e87a28dc4af36a59b5d99f51dc3ae203b5
Oscar <lagartoverde97@gmail.com>
2021-12-02 12:21:51 +0100

    new dieter design solves problems with powerselect

--- addon/utils/locatieOptions.js
+++ addon/utils/locatieOptions.js
@@ -1,18 +1,23 @@
 export const LOCATIE_OPTIONS = [
   {
     label: 'Op het kruispunt van de ${placeholder} met de ${placeholder} geldt',
+    value: 1,
   },
   {
     label: 'Op de  ${placeholder} ter hoogte van  ${placeholder} geldt',
+    value: 2,
   },
   {
     label: 'Op de ${placeholder} tot {placeholder} geldt',
+    value: 3,
   },
   {
     label:
       'Op de ${placeholder} vanaf ${placeholder} in de richting van ${placeholder} geldt',
+    value: 4,
   },
   {
     label: 'Op alle wegen die uitkomen op ${placeholder} geldt',
+    value: 5,
   },
 ];
